300b09692046bfce6d286fc8435cdaeade53e273
hibohiboo <swdice66@gmail.com>
2021-02-11 21:35:05 +0900

    fix config file AB#40

--- src/lib/firebase/initFirebase.ts
+++ src/lib/firebase/initFirebase.ts
@@ -1,12 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
-
-const config = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-}
+import config from './config'
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
