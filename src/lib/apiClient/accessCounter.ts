9b39986e69be9f0653237be3b856297f3379c26e
hibohiboo <swdice66@gmail.com>
2021-03-07 00:56:36 +0900

    リファクタリング

--- src/lib/apiClient/accessCounter.ts
+++ src/lib/apiClient/accessCounter.ts
@@ -1,8 +1,10 @@
 import * as t from "io-ts";
 import { typedDecoder } from './decoder'
 import { fetcher, JsonFetcher } from './fetcher'
+import type { AccessNumber } from '~/types/openapi'
 
 const TAccessNumber = t.exact(t.type({ accessNumber: t.number }));
 const accessNumberFetcher = () => fetcher(process.env.NEXT_PUBLIC_GAS_ACCESS_NUMBER, { method: 'POST' })
 
-export const fetchAccessNumber = async (fetchJson: JsonFetcher =  accessNumberFetcher) => typedDecoder(TAccessNumber, await fetchJson())
+export const fetchAccessNumber =
+    async (fetchJson: JsonFetcher =  accessNumberFetcher): Promise<AccessNumber> => typedDecoder(TAccessNumber, await fetchJson())
