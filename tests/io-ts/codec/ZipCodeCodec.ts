9269b9eb50b3617f8c888ccfdf72d36ff17c2056
hibohiboo <swdice66@gmail.com>
2021-03-09 00:46:34 +0900

    郵便番号を数値に変更 AB#56

--- tests/io-ts/codec/ZipCodeCodec.ts
+++ tests/io-ts/codec/ZipCodeCodec.ts
@@ -1,16 +1,15 @@
 import * as t from 'io-ts'
 import { isRight, map, right } from 'fp-ts/lib/Either'
 import { pipe } from 'fp-ts/lib/pipeable'
-// https://www.puzzle.ch/de/blog/articles/2019/09/25/data-contracts-and-transformations-with-io-ts
-type ZipCode = string
+
+type ZipCode = number
 const RegexZipCode = /^(\d{3})-?(\d{4})$/
 
 const isZipCode = (input: unknown): input is ZipCode =>
   RegexZipCode.test(input.toString())
 
-const formatZipCode = (input: ZipCode) => {
-  const [, first, last] = RegexZipCode.exec(input.toString())
-  return `${first}-${last}`
+const formatZipCode = (input: unknown) => {
+  return Number(input.toString().replace('-', ''))
 }
 export const ZipCodeCodec = new t.Type<ZipCode, string, unknown>(
   // a unique name for this codec
@@ -25,5 +24,8 @@ export const ZipCodeCodec = new t.Type<ZipCode, string, unknown>(
       : t.failure(input, context, `郵便番号の形式が不正です。 : ${input}`),
   // converts a value of type A to a value of type O
   // `A` and `O` are the same, so `encode` is just the identity function
-  t.identity,
+  (a: ZipCode) => {
+    const [, first, last] = RegexZipCode.exec(a.toString())
+    return `${first}-${last}`
+  },
 )
--- tests/io-ts/kotaIoTs.spec.ts
+++ tests/io-ts/kotaIoTs.spec.ts
@@ -5,33 +5,33 @@ import { PhoneNumberCodec } from './codec/PhoneNumberCodec'
 
 describe('郵便番号チェック', () => {
   test('郵便番号が正しい場合 ハイフンあり', () => {
-    const x = ZipCodeCodec.decode(JSON.parse(`"000-0000"`))
+    const x = ZipCodeCodec.decode(JSON.parse(`"509-0000"`))
     if (x._tag === 'Right') {
-      expect(x.right).toStrictEqual('000-0000')
+      expect(x.right).toStrictEqual(5090000)
     }
     // 上記と同じ意味となる
-    expect(x).toStrictEqual(right('000-0000'))
+    expect(x).toStrictEqual(right(5090000))
   })
-  test('郵便番号が正しい場合 ハイフンなし フォーマットされること', () => {
-    const x = ZipCodeCodec.decode(JSON.parse(`"0000000"`))
-    expect(x).toStrictEqual(right('000-0000'))
+  test('郵便番号が正しい場合 ハイフンなし', () => {
+    const x = ZipCodeCodec.decode(JSON.parse(`"5090000"`))
+    expect(x).toStrictEqual(right(5090000))
   })
   test('エンコードできること', () => {
-    const x = ZipCodeCodec.decode(JSON.parse(`"0000000"`))
+    const x = ZipCodeCodec.decode(JSON.parse(`"5090000"`))
     if (x._tag === 'Right') {
-      expect(ZipCodeCodec.encode(x.right)).toStrictEqual('000-0000')
+      expect(ZipCodeCodec.encode(x.right)).toStrictEqual('509-0000')
     } else {
       expect(false).toBeTruthy()
     }
   })
   test('郵便番号が不正な場合に失敗すること', () => {
-    const x = ZipCodeCodec.decode(JSON.parse(`"000--0000"`))
+    const x = ZipCodeCodec.decode(JSON.parse(`"509--0000"`))
     expect(isRight(x)).toBeFalsy()
   })
   test('郵便番号が不正な場合にエラーメッセージを取得できること', () => {
-    const x = ZipCodeCodec.decode(JSON.parse(`"000--0000"`))
+    const x = ZipCodeCodec.decode(JSON.parse(`"509--0000"`))
     expect(PathReporter.report(x)).toStrictEqual([
-      '郵便番号の形式が不正です。 : 000--0000',
+      '郵便番号の形式が不正です。 : 509--0000',
     ])
   })
 })
@@ -39,10 +39,6 @@ describe('郵便番号チェック', () => {
 describe('電話番号チェック', () => {
   test('電話番号が正しい場合 ハイフンあり', () => {
     const x = PhoneNumberCodec.decode(JSON.parse(`"0000-00-0000"`))
-    if (x._tag === 'Right') {
-      expect(x.right).toStrictEqual('0000-00-0000')
-    }
-    // 上記と同じ意味となる
     expect(x).toStrictEqual(right('0000-00-0000'))
   })
   test('電話番号が正しい場合 ハイフンなし フォーマットされること', () => {
