440e9712313cb755de4e53dd4217e0843f15ad72
hibohiboo <swdice66@gmail.com>
2021-07-22 16:48:05 +0900

    fix index AB#85

--- src/pages/github/index.tsx
+++ src/pages/github/index.tsx
@@ -1,15 +1,7 @@
-import { InferGetStaticPropsType } from 'next'
-import { octokit } from '~/domain/github/fetcher'
-
-type PageProps = InferGetStaticPropsType<typeof getStaticProps>
-export const getStaticProps = async () => {
-  const repos = await octokit.request(`GET /users/{username}/repos`, {
-    username: process.env.GITHUB_USER,
-  })
-  return { props: { repos } }
-}
-export default function Page(props: PageProps) {
-  if (!props.repos.data) return <>error!</>
-  console.log(props.repos)
-  return <div>Hello Next.js</div>
+import { Template } from '~/domain/webdb-sample/components/templates'
+import React from 'react'
+// ___________________________________________________________________________
+//
+export default function Page() {
+  return <Template />
 }
